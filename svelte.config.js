f1e3f289ec82b7391c9f2c5978c8d1be99741ca4
Puru Vijay <devpuruvj@gmail.com>
2021-06-17 10:08:15 +0530

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import preprocess from 'svelte-preprocess';
+import staticAdapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: staticAdapter(),
+    target: '#svelte',
+  },
+};
+
+export default config;
